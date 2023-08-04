f17aaab993b025b9e54521c5c1922c8d120d689b
Chunyang Shen <scy0208@gmail.com>
2023-08-03 18:11:56 -0700

    Add documentation

---
+++ src/components/documentation.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import axios from 'axios';
+
+const Documentation: React.FC = () => {
+  const [markdownContent, setMarkdownContent] = useState('');
+
+  useEffect(() => {
+    // Fetch the Markdown file from the public directory
+    axios.get('/readme.md').then((response) => {
+      setMarkdownContent(response.data);
+    }).catch((error) => {
+      console.error('An error occurred while loading the document:', error);
+    });
+  }, []);
+
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+      <ReactMarkdown
+        children={markdownContent}
+        components={{
+          code: ({ node, className, ...props }) => (
+            <pre style={{ background: 'black', padding: '15px', borderRadius: '5px' }}>
+              <code style={{ color: 'white' }} className={className} {...props} />
+            </pre>
+          ),
+          h1: ({ node, ...props }) => <h1 style={{ fontSize: '2rem', borderBottom: '1px solid #ccc' }} {...props} />,
+          h2: ({ node, ...props }) => <h2 style={{ fontSize: '1.75rem' }} {...props} />,
+          h3: ({ node, ...props }) => <h3 style={{ fontSize: '1.5rem' }} {...props} />,
+          // Add more styling for other elements as needed
+        }}
+      />
+    </div>
+  );
+};
+
+export default Documentation;
---
+++ src/pages/documentation.tsx
@@ -0,0 +1,12 @@
+import Documentation from '@/components/documentation';
+
+const SomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Documentation Page</h1>
+      <Documentation />
+    </div>
+  );
+};
+
+export default SomePage;
